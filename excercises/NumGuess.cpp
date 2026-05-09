1ed79b8de43caca3daeed3fd7dbb56bb5703a53e
Manthan Kumar <manthanlearn@gmail.com>
2026-05-09 17:02:41 +0530

    number guessing

---
+++ excercises/NumGuess.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+int main () {
+    int number, guess,tries = 0;
+
+    srand(time(NULL)); // seed the random number generator
+    number = rand() % 100 + 1; // generate a random number between
+    std::cout<<"*****************Number Guessing Game******************" << std::endl;
+    
+    do {
+        std::cout<<"enter a guess between 1 and 100: ";
+        std::cin >> guess;
+        tries++;
+        if (guess > number) {
+            std::cout<<"too high! try again."<<std::endl;
+        }
+        else if (guess < number) {
+            std::cout<<"too low! try again."<<std::endl;
+        }
+        else {
+            std::cout<<"congratulations! you guessed the number in "<<tries<<" tries."<<std::endl;
+        }
+    } while (guess != number);
+
+
+    return 0;
+}
+
